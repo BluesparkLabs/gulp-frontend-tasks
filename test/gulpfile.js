6b5fa9276255ca7c9936c0234456006a2e92371b
James Wilson <jrwilson3@gmail.com>
2018-05-31 12:16:22 -0500

    Add test folder with basic sass test. #8

---
+++ test/gulpfile.js
@@ -0,0 +1,6 @@
+// Note: gulp-frontend-tasks is pulled in from the parent directory (../) for
+// testing purposes only. This gulpfile.js should not be used as a guide for
+// configuring your projects.  The README.md contains proper guidelines for
+// configuring real projects.
+var gulp = require('gulp');
+require('../')(gulp);
